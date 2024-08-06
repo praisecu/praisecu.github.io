446fd5d3c601b47810de881934398b8710564a92
Chahat Deep Singh <chahatdeepsingh@gmail.com>
2024-08-06 12:23:14 -0600

    Update footer.js
    Address added!

--- js/footer.js
+++ js/footer.js
@@ -72,7 +72,7 @@ footerTemplate.innerHTML = `
         <div class="praise-header"><a href="/" title="PRAISe at CU Boulder" style="color:black; font-size:60px">PRAISe Lab</div></a>
 				<br>
 				<h5>
-					Perception, Robotics, AI and Sensing Lab<br>University of Colorado, Boulder<br>Copyright &#169 2024</font>
+					Lab: ECES 1B14 and DLC 2b40 | Office: ECES 152<br>Perception, Robotics, AI and Sensing Lab<br>University of Colorado, Boulder<br>Copyright &#169 2024</font>
 					<hr width="30%">
 					<!-- Auto Year, Doesn't work in JS while loading header/footer from template -->
 					<!--<script type="text/javascript">document.write( new Date().getFullYear() );</script>-->
